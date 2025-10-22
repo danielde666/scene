c790d8b1a2adc2a38056f9a37af684d445962427
danny de <danielde666@users.noreply.github.com>
2025-10-22 15:43:49 -0700

    Update HeroSection.js

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -4,7 +4,7 @@ export default function HeroSection() {
     return (
       <section className="min-h-screen flex flex-col items-center justify-center text-white text-center bg-black">
        
-       <h1>SCENE/h1>
+       <h1>SCENE</h1>
 
 
        <NewsletterSignup />
